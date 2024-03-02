d1f0062ab74fb76c0394ade35a3b45f413332906
Eric Sia Siew Wei <136912487+EricSiaSiewWei@users.noreply.github.com>
2024-03-02 12:10:53 +0800

    Update YOLOv8 Live Webcam Tracker.py

--- yolov8/YOLOv8 Live Webcam Tracker.py
+++ yolov8/YOLOv8 Live Webcam Tracker.py
@@ -15,7 +15,7 @@ def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="YOLOv8 live")
     parser.add_argument(
         "--webcam-resolution", 
-        default=[1280, 720], 
+        default=[640, 480], 
         nargs=2, 
         type=int
     )
